3b62c6e3756e1b191f4a8428e1a524ff72257c89
Aniruddha Adhikary (Ani) <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-07 23:59:10 +0800

    chore: disable output mode

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
   },
   basePath: process.env.GITHUB_ACTIONS ? '/mrt-buddy-web' : '',
   assetPrefix: process.env.GITHUB_ACTIONS ? '/mrt-buddy-web/' : '',
   trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/privacy-poilcy.html',
+        destination: '/privacy-policy',
+      },
+      {
+        source: '/privacy-policy.html',
+        destination: '/privacy-policy',
+      },
+      {
+        source: '/contributors.html',
+        destination: '/contributors',
+      },
+    ];
+  },
 }
