848f121224d6015ae2925b293930581c4adaae6b
Aniruddha Adhikary (Ani) <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-07 23:52:05 +0800

    chore: redirect from old urls

--- next.config.js
+++ next.config.js
@@ -8,20 +8,4 @@ module.exports = {
   basePath: process.env.GITHUB_ACTIONS ? '/mrt-buddy-web' : '',
   assetPrefix: process.env.GITHUB_ACTIONS ? '/mrt-buddy-web/' : '',
   trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/privacy-poilcy.html',
-        destination: '/privacy-policy',
-      },
-      {
-        source: '/privacy-policy.html',
-        destination: '/privacy-policy',
-      },
-      {
-        source: '/contributors.html',
-        destination: '/contributors',
-      },
-    ];
-  },
 }
