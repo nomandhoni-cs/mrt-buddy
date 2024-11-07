a12e941a5fec9cef3490d796f60d32a147ff42c3
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-07 16:41:46 +0800

    chore: add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Noto_Sans } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const notoSans = Noto_Sans({
   subsets: ['latin'],
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={notoSans.className}>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-1YW6R1YDTY" />
     </main>
   )
 }
