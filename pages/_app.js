5f83a928818ff8a3d91cf09e755c266b688124aa
Aniruddha Adhikary (Ani) <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-08 08:28:39 +0000

    chore: change cover photo

--- pages/_app.js
+++ pages/_app.js
@@ -25,9 +25,11 @@ export default function App({ Component, pageProps }) {
 
         <meta property="og:url" content="https://mrtbuddy.com/" />
         <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="MRT Buddy" />
+        <meta property="og:locale" content="en_US" />
         <meta property="og:title" content="MRT Buddy - Your Dhaka Metro Rail Companion App" />
         <meta property="og:description" content="MRT Buddy is an unofficial community-driven Android app designed to check the balance of your Dhaka MRT Card. It reads the last 10 transactions from the RapidPass / MRT Card. No internet connectivity required. This app is not affiliated with DMTCL, JICA, Government of Bangladesh or any of its affiliates" />
-        <meta property="og:image" content="/card.jpeg" />
+        <meta property="og:image" content="/cover.png" />
 
         {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
@@ -35,7 +37,7 @@ export default function App({ Component, pageProps }) {
         <meta property="twitter:url" content="https://mrtbuddy.com/" />
         <meta name="twitter:title" content="MRT Buddy - Your Dhaka Metro Rail Companion App" />
         <meta name="twitter:description" content="MRT Buddy is an unofficial community-driven Android app designed to check the balance of your Dhaka MRT Card. It reads the last 10 transactions from the RapidPass / MRT Card. No internet connectivity required. This app is not affiliated with DMTCL, JICA, Government of Bangladesh or any of its affiliates" />
-        <meta property="twitter:image" content="/card.jpeg" />
+        <meta property="twitter:image" content="/cover.png" />
 
       </Head>
       <Component {...pageProps} />
