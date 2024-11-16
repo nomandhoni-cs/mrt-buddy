56db6400981aea629d6a9647116fb4feb48af6db
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-17 01:05:58 +0800

    chore: fix open source background color on dark mode

--- composeApp/src/commonMain/kotlin/net/adhikary/mrtbuddy/ui/screens/licenses/OpenSourceLicensesScreen.kt
+++ composeApp/src/commonMain/kotlin/net/adhikary/mrtbuddy/ui/screens/licenses/OpenSourceLicensesScreen.kt
@@ -1,5 +1,6 @@
 package net.adhikary.mrtbuddy.ui.screens.licenses
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Icon
@@ -11,6 +12,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewStateWithHTMLFile
 
@@ -34,7 +36,7 @@ fun OpenSourceLicensesScreen(
             val webViewState = rememberWebViewStateWithHTMLFile("open-source-licenses.html")
             WebView(
                 state = webViewState,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier.fillMaxSize().background(Color.White)
             )
         }
     }
